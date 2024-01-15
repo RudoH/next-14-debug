8ac67bcb2377978ca1477b3c30c2d80cbea96dc5
Rudo Hengst <rudohengst@gmail.com>
2024-01-15 16:09:46 -0500

    Add cache control no store

--- src/pages/listing/[id]/[[...slug]].js
+++ src/pages/listing/[id]/[[...slug]].js
@@ -1,5 +1,8 @@
 // Return a test string as a prop from getServersideProps
 export async function getServerSideProps() {
+  // set caching headers to avoid caching in the browser
+  context.res.setHeader("Cache-Control", "no-store");
+
   return {
     props: {
       mlsNumber: "1234566",
